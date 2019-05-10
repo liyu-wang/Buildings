d7d5b243eab900c37571e1a667bd668e3238f5bd
Liyu Wang <liyu.wang@oasis.com>
2019-05-10 21:28:37 +1000

    initial project

---
+++ Buildings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buildings
+//
+//  Created by Liyu Wang on 10/5/19.
+//  Copyright © 2019 Liyu Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
