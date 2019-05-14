63d19bdfe8e2469586c3abdc324af858009ba7a9
Liyu Wang <liyu.wang@oasis.com>
2019-05-15 00:03:59 +1000

    implemented the filter logic and made the building list reactive to the filters

--- Buildings/ViewModels/BuildingsViewModel.swift
+++ Buildings/ViewModels/BuildingsViewModel.swift
@@ -23,6 +23,47 @@ class BuildingsViewModel {
     
     init(buildingWebService: BuildingsWebService = BuildingsWebServiceImpl()) {
         self.buildingWebService = buildingWebService
+        
+        Observable.combineLatest(FiltersInMemoryStore.shared.selectedCountries, FiltersInMemoryStore.shared.selectedCities)
+            .subscribe(
+                onNext: { countryFilters, cityFilters in
+                    self.applyFilters(counties: countryFilters, cities: cityFilters)
+                }
+            )
+            .disposed(by: bag)
+    }
+    
+}
+
+extension BuildingsViewModel {
+    
+    private func saveFilters(from buildings: [Building]) {
+        var citySet = Set<String>()
+        var countrySet = Set<String>()
+        for building in buildings {
+            citySet.insert(building.address.country)
+            countrySet.insert(building.address.city)
+        }
+        
+        FiltersInMemoryStore.shared.save(countries: citySet, cities: countrySet)
+    }
+    
+    private func applyFilters(counties: Set<String>, cities: Set<String>) {
+        var array : [Building] = []
+        if counties.count > 0 && cities.count > 0 {
+            // both country and city filters are active
+            array = self.allBuildings.filter { counties.contains($0.address.country) && cities.contains($0.address.city) }
+        } else if counties.count > 0 && cities.count == 0 {
+            // only country filter is active
+            array = self.allBuildings.filter { counties.contains($0.address.country) }
+        } else if counties.count == 0 && cities.count > 0 {
+            // only city filter is active
+            array = self.allBuildings.filter { cities.contains($0.address.city) }
+        } else if counties.count == 0 && cities.count == 0 {
+            // no filters applied should show all buildings
+            array = self.allBuildings
+        }
+        self.buildings.accept(array)
     }
     
 }
@@ -53,31 +94,6 @@ extension BuildingsViewModel {
     
     func building(at index: Int) -> Building {
         return self.buildings.value[index]
-    }
-    
-    func applyFilters(counties: Set<String>, cities: Set<String>) {
-        if counties.count == 0 && cities.count == 0 {
-            // no filters applied should show all buildings
-            self.buildings.accept(self.allBuildings)
-        } else {
-            let array = self.allBuildings.filter { counties.contains($0.address.country) && cities.contains($0.address.city) }
-            self.buildings.accept(array)
-        }
-    }
-    
-}
-
-extension BuildingsViewModel {
-    
-    private func saveFilters(from buildings: [Building]) {
-        var citySet = Set<String>()
-        var countrySet = Set<String>()
-        for building in buildings {
-            citySet.insert(building.address.country)
-            countrySet.insert(building.address.city)
-        }
-
-        FiltersInMemoryStore.shared.save(countries: citySet, cities: countrySet)
     }
     
 }
