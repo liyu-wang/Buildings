ad78f2e1591aacdfe3217e58f00feadcbc37031c
Liyu Wang <liyu.wang@oasis.com>
2019-05-17 23:43:50 +1000

    use RxBlocking for testing Rx code

--- BuildingsTests/BuildingsTests.swift
+++ BuildingsTests/BuildingsTests.swift
@@ -9,14 +9,13 @@
 import XCTest
 import RxSwift
 import RxCocoa
+import RxBlocking
 @testable import Buildings
 
 class BuildingsTests: XCTestCase {
 
     private let buildingsViewModel = BuildingsViewModel(buildingWebService: MockBuidlingsWebServiceImpl())
-    private let bag = DisposeBag()
-    
-    private var disposable: Disposable?
+    private var bag = DisposeBag()
     
     override func setUp() {
         // load buildings via MockBuidlingsWebServiceImpl from local json file in the app bundle
@@ -24,7 +23,8 @@ class BuildingsTests: XCTestCase {
     }
 
     override func tearDown() {
-        self.disposable?.dispose()
+        // dispose all disposables
+        self.bag = DisposeBag()
         
         // clear countries and cities in in-memory db
         FiltersInMemoryStore.shared.save(countries: [], cities: [])
@@ -33,8 +33,7 @@ class BuildingsTests: XCTestCase {
     }
 
     func testMockServiceLoadingSuccess() {
-        
-        self.disposable = buildingsViewModel.buildings
+        buildingsViewModel.buildings
             .subscribe(
                 onNext: { buildings in
                     // 4 buildings in total
@@ -44,13 +43,14 @@ class BuildingsTests: XCTestCase {
                     XCTFail()
                 }
             )
+            .disposed(by: bag)
     }
     
     func testCountryFilterOnly() {
         // apply country filter
         FiltersInMemoryStore.shared.save(selectedSountries: ["Germany"], selectedeCities: [])
         
-        self.disposable = buildingsViewModel.buildings
+        buildingsViewModel.buildings
             .subscribe(
                 onNext: { buildings in
                     // only 1 building in Germany
@@ -60,38 +60,81 @@ class BuildingsTests: XCTestCase {
                     XCTFail()
                 }
             )
+            .disposed(by: bag)
     }
     
     func testCityFilterOnly() {
         // apply country filter
         FiltersInMemoryStore.shared.save(selectedSountries: [], selectedeCities: ["Sydney"])
         
-        self.disposable = buildingsViewModel.buildings
+        buildingsViewModel.buildings
             .subscribe(
                 onNext: { buildings in
                     // 2 buildings in sydney
                     XCTAssert(buildings.count == 2)
-            },
+                },
                 onError: { error in
                     XCTFail()
-            }
-        )
+                }
+            )
+            .disposed(by: bag)
     }
     
-    func testCountryAndCityFilter() {
+    func testCountryAndCityFilters() {
         // apply country filter
         FiltersInMemoryStore.shared.save(selectedSountries: ["Germany"], selectedeCities: ["Sydney"])
         
-        self.disposable = buildingsViewModel.buildings
+        buildingsViewModel.buildings
             .subscribe(
                 onNext: { buildings in
                     // no buildings in sydney Germany
                     XCTAssert(buildings.count == 0)
-            },
+                },
                 onError: { error in
                     XCTFail()
-            }
-        )
+                }
+            )
+            .disposed(by: bag)
+    }
+    
+}
+
+extension BuildingsTests {
+    
+    func testCountryAndCityFiltersWithBlocking() {
+        // apply country filter
+        FiltersInMemoryStore.shared.save(selectedSountries: ["Germany"], selectedeCities: ["Sydney"])
+        
+        do {
+            let result = try buildingsViewModel.buildings.toBlocking().first()
+            XCTAssertEqual(result?.count, 0)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
+    func testClearFilters() {
+        let observableToTest = buildingsViewModel.buildings
+        
+        // apply filters which causes 0 building showing
+        FiltersInMemoryStore.shared.save(selectedSountries: ["Germany"], selectedeCities: ["Sydney"])
+        
+        do {
+            let result = try observableToTest.toBlocking().first()
+            XCTAssertEqual(result?.count, 0)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+        
+        // clear all filters which should show all buildings
+        FiltersInMemoryStore.shared.save(selectedSountries: [], selectedeCities: [])
+        
+        do {
+            let result = try observableToTest.toBlocking().first()
+            XCTAssertEqual(result?.count, 4)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
 }
